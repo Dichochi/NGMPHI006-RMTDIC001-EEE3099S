f31b6fe2302bd4d33d99570fbc82b545eef3089e
TitoBOSS <ngmphi006@myuct.ac.za>
2024-10-18 01:40:55 +0200

    Final code before the day of the demo

---
+++ MicroMouseFiles/MicroMouseTemplate_ert_rtw/instrumented/MicroMouseTemplate_private.h
@@ -0,0 +1,92 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: MicroMouseTemplate_private.h
+ *
+ * Code generated for Simulink model 'MicroMouseTemplate'.
+ *
+ * Model version                  : 3.53
+ * Simulink Coder version         : 24.1 (R2024a) 19-Nov-2023
+ * C/C++ source code generated on : Fri Oct 18 01:16:30 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef MicroMouseTemplate_private_h_
+#define MicroMouseTemplate_private_h_
+#include "rtwtypes.h"
+#include "MicroMouseTemplate.h"
+#include "MicroMouseTemplate_types.h"
+#ifndef UCHAR_MAX
+#include <limits.h>
+#endif
+
+#if ( UCHAR_MAX != (0xFFU) ) || ( SCHAR_MAX != (0x7F) )
+#error Code was generated for compiler with different sized uchar/char. \
+Consider adjusting Test hardware word size settings on the \
+Hardware Implementation pane to match your compiler word sizes as \
+defined in limits.h of the compiler. Alternatively, you can \
+select the Test hardware is the same as production hardware option and \
+select the Enable portable word sizes option on the Code Generation > \
+Verification pane for ERT based targets, which will disable the \
+preprocessor word size checks.
+#endif
+
+#if ( USHRT_MAX != (0xFFFFU) ) || ( SHRT_MAX != (0x7FFF) )
+#error Code was generated for compiler with different sized ushort/short. \
+Consider adjusting Test hardware word size settings on the \
+Hardware Implementation pane to match your compiler word sizes as \
+defined in limits.h of the compiler. Alternatively, you can \
+select the Test hardware is the same as production hardware option and \
+select the Enable portable word sizes option on the Code Generation > \
+Verification pane for ERT based targets, which will disable the \
+preprocessor word size checks.
+#endif
+
+#if ( UINT_MAX != (0xFFFFFFFFU) ) || ( INT_MAX != (0x7FFFFFFF) )
+#error Code was generated for compiler with different sized uint/int. \
+Consider adjusting Test hardware word size settings on the \
+Hardware Implementation pane to match your compiler word sizes as \
+defined in limits.h of the compiler. Alternatively, you can \
+select the Test hardware is the same as production hardware option and \
+select the Enable portable word sizes option on the Code Generation > \
+Verification pane for ERT based targets, which will disable the \
+preprocessor word size checks.
+#endif
+
+#if ( ULONG_MAX != (0xFFFFFFFFU) ) || ( LONG_MAX != (0x7FFFFFFF) )
+#error Code was generated for compiler with different sized ulong/long. \
+Consider adjusting Test hardware word size settings on the \
+Hardware Implementation pane to match your compiler word sizes as \
+defined in limits.h of the compiler. Alternatively, you can \
+select the Test hardware is the same as production hardware option and \
+select the Enable portable word sizes option on the Code Generation > \
+Verification pane for ERT based targets, which will disable the \
+preprocessor word size checks.
+#endif
+
+/* Skipping ulong_long/long_long check: insufficient preprocessor integer range. */
+extern real_T rt_roundd(real_T u);
+extern void MicroMouseTe_MATLABSystem1_Init(DW_MATLABSystem1_MicroMouseTe_T
+  *localDW, P_MATLABSystem1_MicroMouseTem_T *localP);
+extern void MicroMouseTemplat_MATLABSystem1(real_T rtu_0,
+  B_MATLABSystem1_MicroMouseTem_T *localB, DW_MATLABSystem1_MicroMouseTe_T
+  *localDW, P_MATLABSystem1_MicroMouseTem_T *localP);
+extern void MicroMouseTe_MATLABSystem3_Init(DW_MATLABSystem3_MicroMouseTe_T
+  *localDW, P_MATLABSystem3_MicroMouseTem_T *localP);
+extern void MicroMouseTemplat_MATLABSystem3(real_T rtu_0,
+  B_MATLABSystem3_MicroMouseTem_T *localB, DW_MATLABSystem3_MicroMouseTe_T
+  *localDW, P_MATLABSystem3_MicroMouseTem_T *localP);
+
+#endif                                 /* MicroMouseTemplate_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
